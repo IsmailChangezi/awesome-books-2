761fd2a1ba6918c5decb9faf71146cbb56b3f3db
Swarzstein <zorrillagja@gmail.com>
2022-10-03 18:04:38 -0400

    Creating and deleting books done

---
+++ main.js
@@ -0,0 +1,48 @@
+
+const addBtn = document.getElementById('add-btn');
+const inpTitle = document.getElementById('title');
+const inpAuthor = document.getElementById('author');
+const bookContainer = document.getElementById("books");
+
+// MY OBJECTS //
+
+
+let Books = new Object();
+let n = 0;
+
+function printBooks() {
+  let printed = '';
+  for(const book in Books) {
+    const bk = Books[book];
+    printed += `
+    <div class="book-section" id="book${bk.id}">
+      <p>${bk.title} by ${bk.author}</p>
+      <button onclick="deleteBook(${bk.id})">Erase</button>
+      <hr/>
+    </div>
+    `;
+  }
+  bookContainer.innerHTML = printed;
+}
+
+function deleteBook(id){
+  for(const book in Books) {
+    const bk = Books[book];
+    if (bk.id === id) {
+      delete Books[book];
+      console.log(Books[book]);
+    }
+  }
+  printBooks();
+}
+
+addBtn.addEventListener("click", () => {
+  n += 1;
+  let str = 'book'+n;
+  Books[str] = {
+    'id': n,
+    'author': inpAuthor.value,
+    'title': inpTitle.value
+  };
+  printBooks();
+});
