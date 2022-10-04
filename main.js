309fc6228faf3282a426c39720cff4fa8c4981c8
Swarzstein <zorrillagja@gmail.com>
2022-10-04 16:32:10 -0400

    printing books ok

--- main.js
+++ main.js
@@ -5,64 +5,89 @@ const bookContainer = document.getElementById('books');
 const storage = JSON.parse(localStorage.getItem('BOOkS'));
 // MY OBJECTS //
 let Books = {};
-
-const storeData = () => {
-  localStorage.setItem('BOOkS', JSON.stringify(Books));
-};
-
-function lastId() {
-  if (localStorage.length > 0) {
-    Books = storage;
-    const last = Object.keys(Books)[Object.keys(Books).length - 1];
-    return Books[last].id;
-    //  eslint-disable-next-line
-  } else {
-    return 0;
+let Biblios = class {
+  constructor(id, title, author) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
   }
-}
 
-let n = lastId();
-
-function printBooks() {
-  let printed = '';
-  //  eslint-disable-next-line
-  for (const book in Books) {
-    const bk = Books[book];
-    printed += `
-    <div class="book-section" id="book${bk.id}">
-      <p>${bk.title} by ${bk.author}</p>
-      <button onclick="deleteBook(${bk.id})">Erase</button>
-      <hr/>
-    </div>
-    `;
-  }
-  bookContainer.innerHTML = printed;
-}
-
-printBooks();
-
-//  eslint-disable-next-line
-function deleteBook(id) {
-  //  eslint-disable-next-line
-  for (const book in Books) { 
-    const bk = Books[book];
-    if (bk.id === id) {
-      delete Books[book];
+  static storeData = () => {
+    localStorage.setItem('BOOkS', JSON.stringify(Books));
+  };
+  
+  static checkStorage(){
+    if (localStorage.length > 0) {
+      Books = storage;
     }
   }
-  printBooks();
-  storeData();
+
+  static lastId() {
+    if (localStorage.length > 0) {
+      Books = storage;
+      const last = Object.keys(Books)[Object.keys(Books).length - 1];
+      return Books[last].id;
+      //  eslint-disable-next-line
+    } else {
+      return 0;
+    }
+  }
+  
+  static printBooks() {
+    let printed = '';
+    //  eslint-disable-next-line
+    for (const book in Books) {
+      const bk = Books[book];
+      printed += `
+      <div class="book-section" id="book${bk.id}">
+        <p>${bk.title} by ${bk.author}</p>
+        <button onclick="deleteBook(${bk.id})">Erase</button>
+        <hr/>
+      </div>
+      `;
+    }
+    bookContainer.innerHTML = printed;
+  }
+  
+  //  eslint-disable-next-line
+  static deleteBook(id) {
+    //  eslint-disable-next-line
+    console.log('I will delete book with id '+id)
+    for (const book in Books) { 
+      const bk = Books[book];
+      if (bk.id === id) {
+        console.log('book found');
+        delete Books[book];
+        console.log('book deleted from books');
+      }
+    }
+    this.printBooks();
+    console.log('books printed');
+    this.storeData();
+    console.log('local storage updated');
+    console.log(storage);
+  }
+  
+  static addBook() {
+    n = this.lastId() +1;
+    console.log('I will add a new book with id '+n);
+    //  eslint-disable-next-line
+    const str = 'book' + n;
+    Books[str] = {
+      id: n,
+      author: inpAuthor.value,
+      title: inpTitle.value,
+    };
+    console.log('book created');
+    this.printBooks();
+    console.log('books printed');
+    this.storeData();
+    console.log('books updated');
+    console.log(storage);
+  }
 }
 
-addBtn.addEventListener('click', () => {
-  n += 1;
-  //  eslint-disable-next-line
-  const str = 'book' + n;
-  Books[str] = {
-    id: n,
-    author: inpAuthor.value,
-    title: inpTitle.value,
-  };
-  printBooks();
-  storeData();
-});
+addBtn.addEventListener('click', Biblios.addBook);
+Biblios.checkStorage();
+Biblios.printBooks();
+
