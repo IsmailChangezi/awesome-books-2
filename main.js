f0ada1a39ef7a17e268211b0cf154d75888f7854
Ismail Ali <ismailchangezi801@gmail.com>
2022-10-05 23:33:33 +0500

    Updated the page by styling it

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const addBtn = document.getElementById('add-btn');
-const inpTitle = document.getElementById('title');
-const inpAuthor = document.getElementById('author');
-const bookContainer = document.getElementById('books');
-localStorage.removeItem('randid');
-const storage = JSON.parse(localStorage.getItem('BOOkS'));
+const addBtn = document.getElementById("add-btn");
+const inpTitle = document.getElementById("title");
+const inpAuthor = document.getElementById("author");
+const bookContainer = document.getElementById("books");
+localStorage.removeItem("randid");
+const storage = JSON.parse(localStorage.getItem("BOOkS"));
 // MY OBJECTS //
 let books = [];
 console.log(storage);
@@ -15,20 +15,20 @@ function checkStorage() {
 }
 checkStorage();
 class Book {
-  constructor (id, title, author) {
+  constructor(id, title, author) {
     this.id = id;
     this.title = title;
     this.author = author;
   }
 
-  Add(){
-    books.push({id: this.id, title: this.title, author: this.author});
+  Add() {
+    books.push({ id: this.id, title: this.title, author: this.author });
   }
-  Delete(id){
+  Delete(id) {
     books = books.filter((item) => {
       for (let i = 0; i < books.length; i += 1) {
         if (item.id === id) {
-          console.log('book found');
+          console.log("book found");
           return false;
         }
         return true;
@@ -38,7 +38,7 @@ class Book {
 }
 
 const storeData = () => {
-  localStorage.setItem('BOOkS', JSON.stringify(books));
+  localStorage.setItem("BOOkS", JSON.stringify(books));
 };
 
 function lastId() {
@@ -52,7 +52,7 @@ function lastId() {
 let n = lastId();
 
 function printBooks() {
-  let printed = '';
+  let printed = "";
   //  eslint-disable-next-line
   for (let i = 0; i < books.length; i += 1) {
     const bk = books[i];
@@ -60,7 +60,7 @@ function printBooks() {
     <div class="book-section" id="book${bk.id}">
       <p>${bk.title} by ${bk.author}</p>
       <button onclick="deleteBook(${bk.id})">Erase</button>
-      <hr/>
+     
     </div>
     `;
   }
@@ -73,28 +73,28 @@ printBooks();
 function deleteBook(id) {
   //let newBooks = [];
   //  eslint-disable-next-line
-  console.log('I will delete book with id ' + id)
+  console.log("I will delete book with id " + id);
   const newBooks = new Book();
   newBooks.Delete(id);
-  console.log('book deleted from books'); 
+  console.log("book deleted from books");
   printBooks();
-  console.log('books printed');
+  console.log("books printed");
   storeData();
-  console.log('local storage updated');
+  console.log("local storage updated");
   console.log(storage);
 }
 
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener("click", () => {
   console.log(books);
   n += 1;
-  console.log('I will add a new book with id ' + n)
+  console.log("I will add a new book with id " + n);
   const newBook = new Book(n, inpTitle.value, inpAuthor.value);
   newBook.Add();
   console.log(books);
-  console.log('book created');
+  console.log("book created");
   printBooks();
-  console.log('books printed');
+  console.log("books printed");
   storeData();
-  console.log('books updated');
+  console.log("books updated");
   console.log(storage);
 });
