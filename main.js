628a037a6ec8b67e54ed8284961e7b4591076b37
Ismail Ali <ismailchangezi801@gmail.com>
2022-10-05 23:53:05 +0500

    Fixed Linter Errors

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-const addBtn = document.getElementById("add-btn");
-const inpTitle = document.getElementById("title");
-const inpAuthor = document.getElementById("author");
-const bookContainer = document.getElementById("books");
-localStorage.removeItem("randid");
-const storage = JSON.parse(localStorage.getItem("BOOkS"));
+const addBtn = document.getElementById('add-btn');
+const inpTitle = document.getElementById('title');
+const inpAuthor = document.getElementById('author');
+const bookContainer = document.getElementById('books');
+localStorage.removeItem('randid');
+const storage = JSON.parse(localStorage.getItem('BOOkS'));
 // MY OBJECTS //
 let books = [];
-console.log(storage);
+
 function checkStorage() {
   if (storage !== null) {
     books = storage;
   }
-  console.log(books);
 }
 checkStorage();
 class Book {
@@ -24,11 +23,12 @@ class Book {
   Add() {
     books.push({ id: this.id, title: this.title, author: this.author });
   }
-  Delete(id) {
+
+  Delete() {
+    //  eslint-disable-next-line
     books = books.filter((item) => {
       for (let i = 0; i < books.length; i += 1) {
-        if (item.id === id) {
-          console.log("book found");
+        if (item.id === this.id) {
           return false;
         }
         return true;
@@ -38,7 +38,7 @@ class Book {
 }
 
 const storeData = () => {
-  localStorage.setItem("BOOkS", JSON.stringify(books));
+  localStorage.setItem('BOOkS', JSON.stringify(books));
 };
 
 function lastId() {
@@ -52,7 +52,7 @@ function lastId() {
 let n = lastId();
 
 function printBooks() {
-  let printed = "";
+  let printed = '';
   //  eslint-disable-next-line
   for (let i = 0; i < books.length; i += 1) {
     const bk = books[i];
@@ -71,30 +71,24 @@ printBooks();
 
 //  eslint-disable-next-line
 function deleteBook(id) {
-  //let newBooks = [];
+  // let newBooks = [];
   //  eslint-disable-next-line
   console.log("I will delete book with id " + id);
-  const newBooks = new Book();
-  newBooks.Delete(id);
-  console.log("book deleted from books");
+  const newBooks = new Book(id, null, null);
+  newBooks.Delete();
+
   printBooks();
-  console.log("books printed");
+
   storeData();
-  console.log("local storage updated");
-  console.log(storage);
 }
 
-addBtn.addEventListener("click", () => {
-  console.log(books);
+addBtn.addEventListener('click', () => {
   n += 1;
-  console.log("I will add a new book with id " + n);
+
   const newBook = new Book(n, inpTitle.value, inpAuthor.value);
   newBook.Add();
-  console.log(books);
-  console.log("book created");
+
   printBooks();
-  console.log("books printed");
+
   storeData();
-  console.log("books updated");
-  console.log(storage);
 });
